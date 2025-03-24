ad838394eef7ebbd6712f04c3b8842a43a091ec3
Hyan <hyansilva177@gmail.com>
2025-03-24 20:12:11 -0300

    correção JWTRequestFilter

---
+++ src/main/java/com/javanauta/agendadortarefas/business/dto/TarefasDTO.java
@@ -0,0 +1,29 @@
+package com.javanauta.agendadortarefas.business.dto;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.javanauta.agendadortarefas.infrastructure.enums.StatusNotificacaoEnum;
+import lombok.*;
+import org.springframework.data.annotation.Id;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.regex.Pattern;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class TarefasDTO {
+
+    @Id // Mongo faz automaticamente
+    private String id;
+    private String nomeTarefa;
+    private String descricao;
+    private LocalDateTime dataCriacao;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm:ss")
+    private LocalDateTime dataEvento;
+    private String emailUsuario;
+    private LocalDate dataAlteracao;
+    private StatusNotificacaoEnum statusNotificacaoEnum;
+}
